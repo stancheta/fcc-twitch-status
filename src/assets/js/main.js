61b3c4ec76e2a48301f00d5120b0eb2e4e8c158d
Steven T Ancheta <steventancheta@gmail.com>
2016-04-03 05:15:43 -0700

    Javascript Breakpoint 1 Expanded skeleton to include all of the functions that I will be using Events for the buttons are completed as is the function to get the data. Next: actually retrieving data for each channel with setTwitchStatuses.

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,20 +1,60 @@
 /* global $ */
 
 var twitchStatus = (function() {
+  var placeholdImage ='http://placehold.it/200x200';
+  var twitchArray = ["freecodecamp", "storbeck", "terakilobyte", "habathcx",
+    "RobotCaleb", "thomasballinger", "noobs2ninjas", "beohoff", "ESL_SC2"];
+  var urlBase =  'https://api.twitch.tv/kraken/streams/';
+  /**
+   * gets data concerning a certain channel using TwitchTV's API
+   * @param {string} url url of the twitch api endpoint
+   * @param {function} callback the function that handles the returned JSONP
+   */
+  function getData(url, callback) {
+    $.getJSON(url, function(result) {
+      callback(result);
+    });
+  }
+  /**
+   *
+   */
+  function setData(data, opt) {
+
+  }
+
+  /**
+   *
+   */
+  function setTwitchStatuses(arr) {
+
+  }
+
   /**
    * Sets the events of the webpage
    */
   function setEvents() {
-    $(".btn-group > .btn").click(function() {
-      $(".btn-group > .btn").removeClass("active");
-      $(this).addClass("active");
+    $(".btn").click(function() {
+      $(".btn").removeClass("active");
+      var $this = $(this);
+      $this.addClass("active");
+      if (($this).html() === 'Online') {
+        $('.app-offline').hide();
+        $('.app-online').show();
+      } else if (($this).html() === 'Offline') {
+        $('.app-online').hide();
+        $('.app-offline').show();
+      } else {
+        $('.app-online, app-offline').show();
+      }
     });
   }
 
   /**
-   * Initializes the
+   * Initializes the starting state of the webpage
    */
   function init() {
+    setTwitchStatuses(twitchArray);
+    $('#all').addClass("active");
     setEvents();
   }
   return {
