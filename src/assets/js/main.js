f74a6a2c82696baf49a8aedecd49e249e66dfa6a
Steven T Ancheta <steventancheta@gmail.com>
2016-04-03 02:23:38 -0700

    CSS pass 2: completely finished and responsive Added an extra em tag in order to hide extra information when the screen width is small (ie phone, bootstrap col-xs). Used media query in order to handle some styles between xs and >sm sizes.
    Added skeleton js.

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,6 +1,25 @@
 /* global $ */
 
-$(".btn-group > .btn").click(function() {
-  $(".btn-group > .btn").removeClass("active");
-  $(this).addClass("active");
-});
+var twitchStatus = (function() {
+  /**
+   * Sets the events of the webpage
+   */
+  function setEvents() {
+    $(".btn-group > .btn").click(function() {
+      $(".btn-group > .btn").removeClass("active");
+      $(this).addClass("active");
+    });
+  }
+
+  /**
+   * Initializes the
+   */
+  function init() {
+    setEvents();
+  }
+  return {
+    init: init
+  };
+})();
+
+twitchStatus.init();
